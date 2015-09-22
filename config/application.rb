ef2051029ff6d90debc62813113e7cda4877153d
Matthew Villwock <mtvillwock@gmail.com>
2015-09-22 14:07:31 -0700

    move dotenv call

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
 
 module LabelBuilder
   class Application < Rails::Application
-    Dotenv::Railtie.load
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
